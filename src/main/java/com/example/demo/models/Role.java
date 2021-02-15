c335c4280e1ec6cfc607f936e54c1d1eafc9da16
mendiliwejden <mendili.wejden@gmail.com>
2021-02-15 10:36:51 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/models/Role.java
@@ -0,0 +1,21 @@
+package com.example.demo.models;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+}
